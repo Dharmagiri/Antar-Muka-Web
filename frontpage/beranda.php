cbcfc3e0228e46419adaa6728488c294820c3cc7
DharmaGiri03 <dhrmgiri@gmail.com>
2021-10-07 13:55:59 +0800

    tugas dharma

---
+++ frontpage/beranda.php
@@ -0,0 +1,62 @@
+<div class="flex flex-col w-full xl:w-2/5 justify-center lg:items-start overflow-y-hidden">
+			<h1 class="my-4 text-3xl md:text-5xl text-black-100 font-bold leading-tight text-center md:text-left slide-in-bottom-h1">BEBBLEX Store "Selling Natural Cane Sugar"</h1>
+			<p class="leading-normal text-base md:text-2xl mb-8 text-center md:text-left slide-in-bottom-subtitle">Pasti Menyediakan Kualitas Gula Tebu Asli dan Harga Bersahabat</p>
+		</div>
+		
+		<!--Right Col-->
+		<div class="w-full xl:w-3/5 py-6 overflow-y-hidden">
+            <img class="w-5/6 mx-auto lg:mr-0 slide-in-bottom" src="asssets/g2.jpg"> 
+		</div>
+
+        <div class="grid grid-cols-2 gap-2">
+			<div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl slide-in-bottom">
+				<div class="md:flex grid grid-cols-2">
+				<div class="md:flex-shrink-0">
+					<img class="h-48 w-full object-cover md:h-full md:w-48" src="asssets/gambar2.jpg" alt="Man looking at item at a store">
+				</div>
+				<div class="p-8">
+					<div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Gula Pasir Tebu</div>
+					<a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline"><button class="bg-green-400 p-1 rounded-lg shadow-md">Booking</button></a>
+					<p class="mt-2 text-gray-500">Gula pasir merupakan bahan baku masakan yang terbuat dari sari tebu dan dikristalkan membentuk serbuk-serbuk seperti pasir.</p>
+				</div>
+				</div>
+			</div>
+			<div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl slide-in-bottom">
+				<div class="md:flex grid grid-cols-2">
+					<div class="md:flex-shrink-0">
+						<img class="h-48 w-full object-cover md:h-full md:w-48" src="asssets/gambar5.jpg">
+					</div>
+					<div class="p-8">
+						<div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Gula Cair Tebu</div>
+						<a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline"><button class="bg-green-400 p-1 rounded-lg shadow-md">Booking</button></a>
+						<p class="mt-2 text-gray-500">Gula Cair Tebu Produk kami di olah dengan melalui tahapan pemurnian yaitu tahapan pengambilan kotoran yang terkandung di dalam nira tebu agar diperoleh produk Gula Cair yang jauh lebih bersih </p>
+					</div>
+					</div>
+			</div>
+			<div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl slide-in-bottom">
+				<div class="md:flex grid grid-cols-2">
+					<div class="md:flex-shrink-0">
+						<img class="h-48 w-full object-cover md:h-full md:w-48" src="asssets/gambar4.jpg">
+					</div>
+					<div class="p-8">
+						<div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Gula Merah Tebu</div>
+						<a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline"><button class="bg-green-400 p-1 rounded-lg shadow-md">Booking</button></a>
+						<p class="mt-2 text-gray-500">Gula batu adalah gula tebu yang tidak melalui tahap kristalisasi. Gula kotak/blok adalah gula kristal lembut yang dipres dalam bentuk dadu.</p>
+					</div>
+					</div>
+			</div>
+			<div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl slide-in-bottom">
+				<div class="md:flex grid grid-cols-2">
+					<div class="md:flex-shrink-0">
+						<img class="h-48 w-full object-cover md:h-full md:w-48" src="asssets/gambar3.jpg">
+					</div>
+					<div class="p-8">
+						<div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Gula Batu Tebu</div>
+						<a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline"><button class="bg-green-400 p-1 rounded-lg shadow-md">Booking</button></a>
+						<p class="mt-2 text-gray-500">Otonan adalah upacara kelahiran yang dilaksanakan oleh masyarakat Bali, Indonesia. Upacara ini dilakanakan berdasarkan kelahiran pada Wuku kalender Bali</p>
+					</div>
+					</div>
+			</div>
+		</div>
+
+		</div>
